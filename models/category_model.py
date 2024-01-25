43fd9d6c7461aec769371c3d989cfff7d5019d89
David09122001 <david.carcer09@gmail.com>
2024-01-25 09:33:15 +0100

    commit

--- models/category_model.py
+++ models/category_model.py
@@ -11,3 +11,14 @@ class Category_model(models.Model):
     product = fields.One2many("truffle_app.product_model","category",string="List of products")
     parent_id = fields.Many2one("truffle_app.category_model",string="Category parent")
     subcategories = fields.One2many("truffle_app.category_model", "parent_id", string="Subcategories")
+    path = fields.Char(string="Path", compute="_compute_path", store=True)
+
+    @api.depends('name', 'parent_id', 'parent_id.path')
+    def _compute_path(self):
+        for category in self:
+            path = category.name
+            parent = category.parent_id
+            while parent:
+                path = f"{parent.name}/{path}" if parent.name else path
+                parent = parent.parent_id
+            category.path = path
--- models/invoice_model.py
+++ models/invoice_model.py
@@ -15,7 +15,8 @@ class Invoice_model(models.Model):
     vat = fields.Selection(string="VAT",selection=[('0','0'),('4','4'),('10','10'),('21','21')], default='0',help="VAT for this invoice")
     total = fields.Float(string="Total",compute="_calculate_total",help="Total invoice",store=True)
     lines = fields.One2many("truffle_app.line_model","reference",string="Lines")
-    client = fields.Char(string="Client", help="The name of the client", required=True)
+   # client = fields.Char(string="Client", help="The name of the client", required=True)
+    client = fields.Many2one("res.partner", string="Client")
     state = fields.Selection(string="Status",selection=[('D','Draft'),('C','Confirmed')], default="D")
 
     def _get_id(self):
@@ -34,5 +35,7 @@ class Invoice_model(models.Model):
 
     def confirmInvoice(self):
         self.state = 'C'
+
+        
 
         
